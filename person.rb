2e7061b331da5aae18e19807c13d38404f4cc6f0
DavyKK <davykk04@gmail.com>
2022-10-14 03:58:28 +0300

    decorator design pattern implemented

--- person.rb
+++ person.rb
@@ -1,5 +1,11 @@
-class Person
+require_relative('nameable')
+require_relative('decorator')
+require_relative('trimmer_decorator')
+require_relative('capitalize_decorator')
+
+class Person < Nameable
   def initialize(age, name = 'unknown', parent_permission: true)
+    super()
     @age = age
     @id = Random.rand(1..1000)
     @name = name
@@ -12,9 +18,20 @@ class Person
     of_age? || @parent_permission ? true : false
   end
 
+  def correct_name
+    @name
+  end
+
   private
 
   def of_age?
     @age >= 18
   end
 end
+
+person = Person.new(22, 'maximilianus')
+person.correct_name
+capitalizedPerson = CapitalizeDecorator.new(person)
+capitalizedPerson.correct_name
+capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
+capitalizedTrimmedPerson.correct_name
